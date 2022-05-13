25dfee90c76411be609ffabb93549891d51135c8
letriluan <75682959+letriluan@users.noreply.github.com>
2022-05-13 09:41:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def main():
                 a = my_data2.reshape(-1, 128, 128, 1)
                 # pass the image through the network to obtain our predictions
                 preds = model.predict(a)
-                if max(preds[0]) <= 0.94:
+                if max(preds[0]) <= 0.96:
                         st.text("THIS IS NOT A FILE OF LUNG DICOM")
                 else:
                         label = classes[np.argmax(preds)]
