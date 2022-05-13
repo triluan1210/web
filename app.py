a835cde62c5de139013e7d4e3d4713f0bb5826bb
Le Tri Luan <letriluan12345@gmail.com>
2022-05-13 08:24:23 +0700

    upload

---
+++ app.py
@@ -0,0 +1,105 @@
+
+import streamlit as st
+import pydicom
+import numpy as np
+import cv2
+from tensorflow import keras
+
+#pydicom.config.convert_wrong_length_to_UN = True
+#pydicom.config.Settings.infer_sq_for_un_vr = True
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+from PIL import Image
+image = Image.open('Capture.PNG')
+
+st.sidebar.image(image,channels="RGB",width =250)
+class WrongFileType(ValueError):
+    pass
+
+def main():
+    st.title("CLASSIFICATION MEDICAL DICOM")
+    #st.sidebar.title("Configuration")
+    st.sidebar.text("YOU ONLY CHOSE LUNG IMAGE")
+    mode1 = st.sidebar.radio(
+        "Select type of input",
+        ('Image', 'Dicom'))
+    if mode1 == 'Dicom':
+        dicom_bytes = st.sidebar.file_uploader("Upload file", type=["dcm","dicom"])
+        # Config
+        print(dicom_bytes)
+        classes = ['Aortic enlargement ', 'Covid', 'Opacity', 'Normal']
+        model = keras.models.load_model('model/VGG16_224_proposed.h5')
+
+        mode = st.sidebar.radio(
+            "Select input source",
+            ('View Image', 'View information'))
+
+        if not dicom_bytes:
+            raise st.stop()
+
+        try:
+            dicom_header = pydicom.read_file(dicom_bytes, force=True)
+            image_dicom = dicom_header.pixel_array/4095
+        except:
+            st.write(WrongFileType("Does not appear to be a DICOM file"))
+
+        if mode == 'View Image':
+
+            if st.sidebar.button("Load Image"):
+                st.image(image_dicom,width =400)
+
+            if st.sidebar.button("Predicted"):
+                st.image(image_dicom,width =400)
+                #print(image_dicom.shape)
+                my_data2 = cv2.resize(image_dicom, (224, 224))
+                a = my_data2.reshape(-1, 224, 224, 1)
+                # pass the image through the network to obtain our predictions
+                preds = model.predict(a)
+                label = classes[np.argmax(preds)]
+                st.text("RESULT : " + label)
+
+        if mode == 'View information' and st.sidebar.button("Load Information"):
+            view = dicom_header.__repr__().split("\n")
+            view = "\n".join(view)
+            f"""
+                ```
+                {view}
+                ```
+                """
+
+    if mode1 == 'Image':
+        image_bytes = st.sidebar.file_uploader("Upload file", type=["jpg", "png"])
+        # Config
+        if image_bytes is not None:
+            classes = ['Covid', 'Normal', 'Pneumonia']
+            model = keras.models.load_model('model/RESNET50_224_image.h5')
+
+            def load_image(img):
+                im = Image.open(img)
+                image = np.array(im)
+                return image
+            image = load_image(image_bytes)
+            image = image/255
+            #print(image.shape)
+            #print(image)
+
+            if st.sidebar.button("Load Image"):
+                    st.image(image_bytes, width=400)
+
+            if st.sidebar.button("Predicted"):
+                    st.image(image_bytes, width=400)
+                    #print(image_bytes.shape)
+                    my_data2 = cv2.resize(image, (224, 224))
+                    #my_data2 = image_bytes/255
+                    a = my_data2.reshape(-1, 224, 224,3)
+                    # pass the image through the network to obtain our predictions
+                    preds = model.predict(a)
+                    label = classes[np.argmax(preds)]
+                    st.text("RESULT : " + label)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except SystemExit:
+        pass
